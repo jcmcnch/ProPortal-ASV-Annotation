f573b44b3aa193a27b829700ca65b6886ceefe1b
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2023-10-17 11:19:01 -0700

    Add summary file; update script

--- scripts/05-QC-SSU-to-remove-contaminating-heterotrophs-etc.sh
+++ scripts/05-QC-SSU-to-remove-contaminating-heterotrophs-etc.sh
@@ -7,7 +7,7 @@ outdir=found-rRNA-SSU-QC
 for item in `ls found-rRNA-SSU-only/*fna` ; do 
 
 	outfile=`basename $item .fna`.classified.SILVA138.1.tsv
-	echo vsearch --sintax $item \
+	vsearch --sintax $item \
 	       	--db /home/db/VSEARCH/220830_SILVA138.1_VSEARCH-formatted.udb \
 	        --tabbedout $outdir/$outfile --threads 10 --sintax_cutoff 0 \
 		--top_hits_only --topn 1 --notrunclabels
