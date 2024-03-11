1fbab10bce5cb90bd6b35b3725d669b089ef6f32
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2024-03-11 11:41:56 -0700

    Fix script which was overwriting new blastdb

--- scripts/06-makeblastdb.sh
+++ scripts/06-makeblastdb.sh
@@ -1,8 +1,4 @@
 #!/bin/bash -i
 conda activate blast-env
 
-mkdir -p blast-db
-
-cat found-rRNA-SSU-only/*fna > blast-db/220123.ProPortal16Sdb.fasta
-
 makeblastdb -dbtype nucl -in blast-db/220123.ProPortal16Sdb.fasta
